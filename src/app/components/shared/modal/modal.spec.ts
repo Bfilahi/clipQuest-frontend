95d02ef302402e4c11af479878742524bf65ed32
Bfilahi <abdelbassete.filahi@gmail.com>
2026-04-11 19:57:58 +0200

    Tested component: modal

--- src/app/components/shared/modal/modal.spec.ts
+++ src/app/components/shared/modal/modal.spec.ts
@@ -1,14 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Modal } from './modal';
+import { ModalService } from '../../../services/modal-service';
+import { Auth } from '../../../services/auth';
+import { By } from '@angular/platform-browser';
 
 describe('Modal', () => {
   let component: Modal;
   let fixture: ComponentFixture<Modal>;
 
+  let mockModalService: jasmine.SpyObj<ModalService>;
+  let mockAuthService: jasmine.SpyObj<Auth>;
+
   beforeEach(async () => {
+    mockModalService = jasmine.createSpyObj(['toggleModal', 'isModalOpen']);
+    mockAuthService = jasmine.createSpyObj(['']);
+
     await TestBed.configureTestingModule({
-      imports: [Modal]
+      imports: [Modal],
+      providers: [
+        {provide: ModalService, useValue: mockModalService},
+        {provide: Auth, useValue: mockAuthService}
+      ]
     })
     .compileComponents();
 
@@ -19,5 +32,51 @@ describe('Modal', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should append element to the body on init', () => {
+    const appendSpy = spyOn(document.body, 'appendChild');
+
+    component.ngOnInit();
+
+    expect(appendSpy).toHaveBeenCalledWith(component.el.nativeElement);
+  });
+
+  it('should call modalService.toggleModal with the correct modalID', () => {
+    component.modalID = 'auth';
+
+    component.closeModal();
+
+    expect(mockModalService.toggleModal).toHaveBeenCalledWith('auth');
+  });
+
+  describe('template', () => {
+    it('should have hidden class when modal is closed', () => {
+      mockModalService.isModalOpen.and.returnValue(false);
+  
+      fixture.detectChanges();
+  
+      const modalEl = fixture.debugElement.query(By.css('#modal')).nativeElement;
+      expect(modalEl.classList.contains('hidden')).toBeTrue();
+    });
+  
+    it('should not have hidden class when modal is open', () => {
+      mockModalService.isModalOpen.and.returnValue(true);
+  
+      fixture.detectChanges();
+  
+      const modalEl = fixture.debugElement.query(By.css('#modal')).nativeElement;
+      expect(modalEl.classList.contains('hidden')).toBeFalse();
+    });
+  
+    it('should close the modal when closeModal is clicked', () => {
+      component.modalID = 'auth';
+      fixture.detectChanges();
+  
+      const btn = fixture.debugElement.query(By.css('button span'));
+      btn.triggerEventHandler('click', null);
+  
+      expect(mockModalService.toggleModal).toHaveBeenCalledWith('auth');
+    });
   });
 });
