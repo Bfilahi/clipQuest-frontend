88f500c7af61deae8abdb264d00ddb48eaaa4eb0
Bfilahi <abdelbassete.filahi@gmail.com>
2026-04-11 21:19:11 +0200

    Tested component: submit-button

--- src/app/components/shared/submit-button/submit-button.spec.ts
+++ src/app/components/shared/submit-button/submit-button.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SubmitButton } from './submit-button';
+import { FormControl, FormGroup } from '@angular/forms';
+
 
 describe('SubmitButton', () => {
   let component: SubmitButton;
@@ -14,10 +16,42 @@ describe('SubmitButton', () => {
 
     fixture = TestBed.createComponent(SubmitButton);
     component = fixture.componentInstance;
+
+    component.form = new FormGroup({
+      test: new FormControl('')
+    });
+
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should disable button when form is invalid', () => {
+    (component.form as FormGroup).setErrors({invalid: true});
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should disable button when form is disabled', () => {
+    (component.form as FormGroup).setErrors({disabled: true});
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTrue();
+  });
+
+  it('should disable button when isDisabled is true', () => {
+    component.isDisabled = true;
+
+    fixture.detectChanges();
+
+    const button = fixture.nativeElement.querySelector('button');
+    expect(button.disabled).toBeTrue();
   });
 });
