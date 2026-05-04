4595a23c4b6026035ed73b20b75a0e59153fcc40
Bfilahi <abdelbassete.filahi@gmail.com>
2026-05-04 18:57:58 +0200

    Tested component: app

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,10 +1,24 @@
 import { TestBed } from '@angular/core/testing';
 import { App } from './app';
+import { ModalService } from './services/modal-service';
+import { Auth } from './services/auth';
+import { provideRouter } from '@angular/router';
 
 describe('App', () => {
+  let mockModalService: jasmine.SpyObj<ModalService>;
+  let mockAuthService: jasmine.SpyObj<Auth>;
+
   beforeEach(async () => {
+    mockModalService = jasmine.createSpyObj(['register', 'unregister']);
+    mockAuthService = jasmine.createSpyObj(['']);
+
     await TestBed.configureTestingModule({
       imports: [App],
+      providers: [
+        provideRouter([]),
+        { provide: ModalService, useValue: mockModalService },
+        { provide: Auth, useValue: mockAuthService }
+      ]
     }).compileComponents();
   });
 
